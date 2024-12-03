2d255cdde69b6e84e5b2840113dbfdce65b8530b
Pontus Hedman <ponhe215@student.liu.se>
2024-12-03 22:25:16 +0100

    Missed some files

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+
+// Adds link functionality to the buttons
+document.getElementById('create-button').onclick = function() {
+    location.href = 'create.html';
+};
+
+document.getElementById('join-button').onclick = function() {
+    location.href = 'join.html';
+};
